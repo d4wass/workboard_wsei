2df3cc8d3fefbafb20a00aa33cc2c538dca471b8
d4wass <dwasilewski.93@gmail.com>
2021-05-27 23:24:41 +0200

    fixing changing grid view function

--- src/components/atoms/Entites/ChangeViewBtn.tsx
+++ src/components/atoms/Entites/ChangeViewBtn.tsx
@@ -1,27 +1,52 @@
 import React, {useState} from 'react';
-import styled from 'styled-components';
 import { Wrapper } from 'utils/Components';
+import styled, {css} from 'styled-components';
 
 const StyledWrapper = styled(Wrapper)`
     border-radius: 10px;
     border: ${({ theme }) => `solid 1px ${theme.color.darkblue}`};
 `;
 
-const StyledButton = styled.button`
-    background-color: transparent;
+const StyledButton = styled.button<{active: boolean}>`
     border: none;
     padding: 10px;
     cursor: pointer;
+    background-color: ${({active}) => active ? 'red' : 'transparent'};
+
 `;
 
 const ChangeViewBtn = ({ handleView }: { handleView: () => void }) => {
 
-    const [] = useState()
+    const [isMosaicActive, setMosaicActive] = useState(true);
+    const [isListActive, setListActive] = useState(false);
+
+    const handleActive = () => {
+        if (isMosaicActive) {
+            setListActive(true)
+            setMosaicActive(false)
+        } else if (!isMosaicActive) {
+            setMosaicActive(true)
+            setListActive(false)
+        }
+    }
 
     return (
         <StyledWrapper>
-            <StyledButton onClick={handleView}>mosaic</StyledButton>
-            <StyledButton onClick={handleView}>list</StyledButton>
+            <StyledButton
+                onClick={(e) => {
+                    console.log(e.target)
+                    handleView()
+                    handleActive()
+                }}
+                active={isMosaicActive}
+            >mosaic</StyledButton>
+            <StyledButton
+                onClick={() => {
+                    handleView()
+                    handleActive()
+                }}
+                active={isListActive}
+            >list</StyledButton>
         </StyledWrapper>
     )
 };
